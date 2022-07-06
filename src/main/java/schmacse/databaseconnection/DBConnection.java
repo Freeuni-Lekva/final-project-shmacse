70ff2c4453ddfb367e8a57b9d727d8b416e502f4
Ioseb Chkhikvadze <iosebi12344@gmail.com>
2022-07-06 14:30:07 +0400

    added mysql connector dependency in pom.xml and implemented database connection

---
+++ src/main/java/schmacse/databaseconnection/DBConnection.java
@@ -0,0 +1,32 @@
+package schmacse.databaseconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static Connection connection;
+
+    public static Connection getConnection() {
+
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/db",
+                    "root", "password");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+
+    public static void closeConnection() {
+
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
