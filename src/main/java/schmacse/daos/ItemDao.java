1f88e7c6f935f2d793f52c12403d70fcb24eeb08
Ioseb Chkhikvadze <iosebi12344@gmail.com>
2022-07-27 15:59:57 +0400

    added method in ItemDao to get list of items by userId

--- src/main/java/schmacse/daos/ItemDao.java
+++ src/main/java/schmacse/daos/ItemDao.java
@@ -5,9 +5,14 @@ import schmacse.model.Item;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ItemDao {
+
+    private static final String SELECT_ITEMS_BY_USER_ID = "SELECT * FROM items WHERE user_id = ?";
 
     private Connection connection;
 
@@ -27,6 +32,48 @@ public class ItemDao {
         stm.setString(3, item.getDescription());
         stm.setObject(4, item.getCategory().name());
         stm.executeUpdate();
+    }
+
+    public List<Item> getItemsByUserId(int userId) throws SQLException {
+
+        List<Item> itemList = new ArrayList<>();
+
+        PreparedStatement stm = connection.prepareStatement(SELECT_ITEMS_BY_USER_ID);
+
+        stm.setInt(1, userId);
+
+        ResultSet resultSet = stm.executeQuery();
+
+        while (resultSet.next()) {
+
+            int id = resultSet.getInt("id");
+            int currUserId = resultSet.getInt("user_id");
+            String name = resultSet.getString("name");
+            String description = resultSet.getString("description");
+            Category category = getCategory(resultSet.getString("category"));
+
+            Item newItem = new Item(id, currUserId, name, description, category);
+
+            itemList.add(newItem);
+        }
+
+        return itemList;
+    }
+
+    private Category getCategory(String category) {
+
+        switch (category.toLowerCase()) {
+            case "trousers":
+                return Category.TROUSERS;
+            case "isgood":
+                return Category.ISGOOD;
+            case "saul":
+                return Category.SAUL;
+            case "anddrive":
+                return Category.ANDDRIVE;
+        }
+
+        return null;
     }
 
 }
