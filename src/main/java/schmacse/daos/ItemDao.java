5f011628fc04c22148c90c349c3015dc2d4c7143
GochaGulua <ggulu19@freeuni.edu.ge>
2022-09-06 23:11:53 +0400

    fixed empty item-table related issue.

--- src/main/java/schmacse/daos/ItemDao.java
+++ src/main/java/schmacse/daos/ItemDao.java
@@ -9,6 +9,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ItemDao {
@@ -204,6 +205,10 @@ public class ItemDao {
 
             Item newItem = new Item(id, userId, itemName, price, description, category);
             filteredList.add(newItem);
+        }
+
+        if (filteredList.size() == 0){
+            return Collections.emptyList();
         }
 
         return filteredList;
