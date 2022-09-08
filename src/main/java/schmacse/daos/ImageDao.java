5a16ced5457815299eaedd2f884bc0d8103261d1
GochaGulua <ggulu19@freeuni.edu.ge>
2022-09-09 03:22:52 +0400

    forgot to add files in previous commit.

---
+++ src/main/java/schmacse/daos/ImageDao.java
@@ -0,0 +1,48 @@
+package schmacse.daos;
+
+import org.eclipse.jdt.internal.compiler.ast.ReturnStatement;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ImageDao {
+    private final Connection connection;
+
+    public ImageDao(Connection connection) {
+        this.connection = connection;
+    }
+
+    public int addImage(int itemId, byte[] image) throws SQLException {
+        PreparedStatement stm = connection.prepareStatement("INSERT INTO item_images (item_id, image) VALUES (?, ?)");
+        stm.setInt(1, itemId);
+        stm.setBytes(2, image);
+        stm.executeQuery();
+        stm.close();
+
+        ResultSet rs = connection.createStatement().executeQuery("SELECT LAST_INSERT_ID()");
+        rs.next();
+        return rs.getInt(1);
+    }
+
+    public void removeWithItemId(int itemId) throws SQLException {
+        connection.createStatement().executeQuery("SET FOREIGN_KEY_CHECKS=0");
+
+        PreparedStatement stm = connection.prepareStatement("DELETE FROM item_images WHERE item_id = ?");
+        stm.setInt(1, itemId);
+        stm.executeQuery();
+        stm.close();
+
+        connection.createStatement().executeQuery("SET FOREIGN_KEY_CHECKS=0");
+    }
+
+    public byte[] getImage(int itemId) throws SQLException {
+        PreparedStatement stm = connection.prepareStatement("SELECT image FROM item_images WHERE id = ?");
+        stm.setInt(1, itemId);
+        ResultSet rs = stm.executeQuery();
+        rs.next();
+
+        return rs.getBytes("image");
+    }
+}
